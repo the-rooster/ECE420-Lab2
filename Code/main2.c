e2bf1065e727790ae798720a6d0074f292f3c43b
the-rooster <andrewmjculberson@gmail.com>
2023-01-23 20:41:53 -0700

    timing stuff

--- Code/main2.c
+++ Code/main2.c
@@ -7,11 +7,15 @@
 #include<unistd.h>
 #include<pthread.h>
 #include "common.h"
-
+#include "timer.h"
 
 char** strings;
 
 pthread_mutex_t* mutexes;
+
+pthread_mutex_t time_write_lock;
+
+
 
 
 void *ServerThread(void *args)
@@ -20,48 +24,74 @@ void *ServerThread(void *args)
     char str[COM_BUFF_SIZE];
     char resp[COM_BUFF_SIZE];
     int ret;
+    double start, end;
+
+    double total_time = 0.0;
+    double transaction_count = 0.0;
+
+
 
     
-    printf("STARTING CLIENT THREAD\n");
+  //printf("STARTING CLIENT THREAD\n");
     ClientRequest* req = malloc(sizeof(ClientRequest));
 
-    printf("HERE?\n");
+  //printf("HERE?\n");
     while(1){
         ret = read(clientFileDescriptor,str,COM_BUFF_SIZE);
 
         // printf("CLIENT SENT: %s\n",str);
-        printf("HERE2?\n");
+      //printf("HERE2?\n");
         if (ret == 0){
-            printf("CONNECTION CLOSED\n");
+          //printf("CONNECTION CLOSED\n");
             //EOF reached
             close(clientFileDescriptor);
-            pthread_exit(NULL);
+
+            // pthread_mutex_lock(&time_write_lock);
+
+            // saveTimes(times,transaction_count);
+
+            // pthread_mutex_unlock(&time_write_lock);
+            if (transaction_count != 0){
+                double* average = malloc(sizeof(double));
+                *average = total_time / transaction_count;
+                pthread_exit(average);
+            }
+            else{
+                pthread_exit(NULL);
+            }
+
         }       
 
         ParseMsg(str,req);
+        
 
-        printf("HERE?3\n");
-        //do mutex stuff here!
-
+        GET_TIME(start);
+        //start timer
         pthread_mutex_lock(&mutexes[req->pos]);
 
-        printf("MADE IT PAST THE LOCK!\n");
+
         if(req->is_read){
             
-            printf("READING\n");
+          //printf("READING\n");
             getContent(resp,req->pos,strings);
         }
         else{
-            printf("WRITING\n");
+          //printf("WRITING\n");
             setContent(req->msg,req->pos,strings);
             memcpy(resp,req->msg,COM_BUFF_SIZE);
         }
         pthread_mutex_unlock(&mutexes[req->pos]);
 
+        GET_TIME(end);
 
-        printf("HERE4?\n");
-        printf("reading from client:%s\n",str);
-        printf("responding with: %s\n",resp);
+        total_time = total_time + (end - start);
+        // printf("TOTAL TIME: %f",total_time);
+        transaction_count = transaction_count + 1;
+
+
+
+      //printf("reading from client:%s\n",str);
+      //printf("responding with: %s\n",resp);
         write(clientFileDescriptor,resp,COM_BUFF_SIZE);
 
 
@@ -103,10 +133,16 @@ int main(int argc, char* argv[])
     int size = atoi(argv[1]);
     int port = atoi(argv[3]);
 
+
+    double avgs[COM_NUM_REQUEST] = {0.0};
+
     //initialize array of strings
     InitStringArray(size);
 
-    printf("STARTING SERVER ON PORT %d WITH STRING ARRAY SIZE %d WITH IP %s\n",port,size,argv[2]);
+    //initialize write lock for saving times
+    pthread_mutex_init(&time_write_lock,NULL);
+
+  //printf("STARTING SERVER ON PORT %d WITH STRING ARRAY SIZE %d WITH IP %s\n",port,size,argv[2]);
 
     struct sockaddr_in sock_var;
     int serverFileDescriptor=socket(AF_INET,SOCK_STREAM,0);
@@ -119,7 +155,7 @@ int main(int argc, char* argv[])
     sock_var.sin_family=AF_INET;
     if(bind(serverFileDescriptor,(struct sockaddr*)&sock_var,sizeof(sock_var))>=0)
     {
-        printf("socket has been created\n");
+      //printf("socket has been created\n");
         listen(serverFileDescriptor,2000); 
         while(1)        //loop infinity
         {
@@ -127,17 +163,31 @@ int main(int argc, char* argv[])
             for(i=0;i<COM_NUM_REQUEST;i++)      //can support COM_NUM_REQUEST threads at a time
             {
                 clientFileDescriptor=accept(serverFileDescriptor,NULL,NULL);
-                printf("Connected to client %d\n",clientFileDescriptor);
+              //printf("Connected to client %d\n",clientFileDescriptor);
                 pthread_create(&t[i],NULL,ServerThread,(void *)(long)clientFileDescriptor);
-                printf("THREAD %d STARTED\n",clientFileDescriptor);
+              //printf("THREAD %d STARTED\n",clientFileDescriptor);
             }
+
+            for(i=0;i<COM_NUM_REQUEST;i++)      //can support COM_NUM_REQUEST threads at a time
+            {
+                double* avg;
+
+                pthread_join(t[i],&avg);
+
+                avgs[i] = *avg;
+
+                free(avg);
+                
+            }
+
+            saveTimes(avgs,COM_NUM_REQUEST);
 
             
         }
         close(serverFileDescriptor);
     }
     else{
-        printf("socket creation failed\n");
+      //printf("socket creation failed\n");
     }
     return 0;
 }
